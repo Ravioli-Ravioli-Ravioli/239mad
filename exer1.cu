04a3c5412d6dff5f3b48a718a4e7c4c298361f5d
Ravioli-Ravioli-Ravioli <elkingmoradoking@gmail.com>
2018-12-10 20:56:12 +0800

    Second

--- exer1.cu
+++ exer1.cu
@@ -3,36 +3,20 @@
 #include <stddef.h>
 #include <time.h>
 
-#define RNC 100
+#define N 10
 
-__global__ void kernel_1t1e(int A[RNC][RNC], int B[RNC][RNC], int C[RNC][RNC], int size){
-   int i = blockIdx.x * blockDim.x + threadIdx.x;
-   int j = blockIdx.y * blockDim.y + threadIdx.y;
-   if (i < size && j < size){
-	   A[i][j] = B[i][j] + C[i][j];
-   }
+__global__ void kernel_1t1e(float A[N][N], float B[N][N], float C[N][N], int size) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < size && j < size){
+        A[i][j] = B[i][j] + C[i][j];
+    }
 }
 
-__global__ void kernel_1t1r(int A[RNC][RNC], int B[RNC][RNC], int C[RNC][RNC], int size){
-   int j = blockIdx.y * blockDim.y + threadIdx.y;
-   for (int k = 0; k < size; k++){
-	if (j < size){
-	     A[k][j] = B[k][j] + C[k][j];	
-	}
-   }
-}
-
-__global__ void kernel_1t1c(int A[RNC][RNC], int B[RNC][RNC], int C[RNC][RNC], int size){
-   int i = blockIdx.x * blockDim.x + threadIdx.x;
-   for (int k = 0; k < size; k++){
-        if (i < size){
-             A[i][k] = B[i][k] + C[i][k]; 
-        }
-   }
-}
-
-int main(void) {
-  int nDevices, i, j, B[RNC][RNC], C[RNC][RNC], A[RNC][RNC], (*A_d)[RNC], (*B_d)[RNC], (*C_d)[RNC] ;
+int main(void){
+  int nDevices;
+  int i, j;
+  float A[N][N], B[N][N], C[N][N], (*A_d)[N], (*B_d)[N], (*C_d)[N];
 
 //////////////////////////////////////Print device properties
 
@@ -44,10 +28,6 @@ int main(void) {
     printf("  Device name: %s\n", prop.name);
     printf("  MaxThreadPerBlock: %d\n",
            prop.maxThreadsPerBlock);
-//    printf("  MultiProcessorCount: %d\n",
-//           prop.multiProcessorCount);
-//    printf("  ClockRate: %d\n",
-//           prop.clockRate);
     printf("  MaxThreadsDim0: %d\n",
            prop.maxThreadsDim[0]);
     printf("  MaxThreadsDim1: %d\n",
@@ -56,56 +36,52 @@ int main(void) {
            prop.maxThreadsDim[2]);
     printf("  MaxGridSize: %d\n",
            prop.maxGridSize[1]);
-//    printf("  Memory Clock Rate (KHz): %d\n",
-//           prop.memoryClockRate);
-//    printf("  Memory Bus Width (bits): %d\n",
-//           prop.memoryBusWidth);
-//    printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
-//           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
   }
 /////////////////////////////////////Populate First Matrix
    srand(1);
-   for (i = 0; i < RNC; i++){
-      for (j = 0; j < RNC; j++) {
-         B[i][j] = rand() % 100 + 1; 
-         printf("%d  ",B[i][j]);
+   for (i = 0; i < N; i++){
+      for (j = 0; j < N; j++) {
+         B[i][j] = ((float)rand()/(float)(RAND_MAX)) * 100;
+         printf("%f  ", B[i][j]);
       }
       printf("\n");
    }
    printf("\n");
 /////////////////////////////////////Populate Second Matrix
-   for (i = 0; i < RNC; i++){
-      for (j = 0; j < RNC; j++) {
-         C[i][j] = rand() % 100 + 1;
-         printf("%d  ",C[i][j]);
+   for (i = 0; i < N; i++){
+      for (j = 0; j < N; j++) {
+         C[i][j] = ((float)rand()/(float)(RAND_MAX)) * 100;
+	 printf("%f  ", C[i][j]);
       }
       printf("\n");
    }
    printf("\n");
    printf("===============================");
    printf("\n");
+
 /////////////////////////////////////Allocate memory in the device
    
-   cudaMalloc((void**) &A_d, (RNC*RNC)*sizeof(int));
-   cudaMalloc((void**) &B_d, (RNC*RNC)*sizeof(int));
-   cudaMalloc((void**) &C_d, (RNC*RNC)*sizeof(int));
+   cudaMalloc((void**) &A_d, (N*N)*sizeof(float));
+   cudaMalloc((void**) &B_d, (N*N)*sizeof(float));
+   cudaMalloc((void**) &C_d, (N*N)*sizeof(float));
 
 ////////////////////////////////////Mem copy from host to device
-   cudaMemcpy(A_d, A, (RNC*RNC)*sizeof(int), cudaMemcpyHostToDevice);
-   cudaMemcpy(B_d, B, (RNC*RNC)*sizeof(int), cudaMemcpyHostToDevice);
-   cudaMemcpy(C_d, C, (RNC*RNC)*sizeof(int), cudaMemcpyHostToDevice);
+   cudaMemcpy(A_d, A, (N*N)*sizeof(float), cudaMemcpyHostToDevice);
+   cudaMemcpy(B_d, B, (N*N)*sizeof(float), cudaMemcpyHostToDevice);
+   cudaMemcpy(C_d, C, (N*N)*sizeof(float), cudaMemcpyHostToDevice);
 
-   dim3 threadsPerBlock(RNC, RNC);
-   dim3 numBlocks(RNC / threadsPerBlock.x, RNC / threadsPerBlock.y);
+   dim3 threadsPerBlock(N, N);
+   dim3 numBlocks(N / threadsPerBlock.x, N / threadsPerBlock.y);
 
    cudaEvent_t start, stop;
    float elapsed = 0;
 
 ////////////////////////////////////ThreadAll
+
    cudaEventCreate(&start);
    cudaEventCreate(&stop);
    cudaEventRecord(start, 0);
-   kernel_1t1e<<<numBlocks,threadsPerBlock>>>(A_d, B_d, C_d, RNC);
+   kernel_1t1e<<<numBlocks,threadsPerBlock>>>(A_d, B_d, C_d, N);
    cudaEventRecord(stop, 0);
 
    cudaEventSynchronize(stop);
@@ -113,11 +89,13 @@ int main(void) {
    cudaEventDestroy(start);
    cudaEventDestroy(stop);
    printf("GPU Run TIme threadsall %.2f ms \n", elapsed);
+
 ////////////////////////////////////Thread Row
+/*
    cudaEventCreate(&start);
    cudaEventCreate(&stop);
    cudaEventRecord(start, 0);
-   kernel_1t1r<<<numBlocks,threadsPerBlock>>>(A_d, B_d, C_d, RNC);
+   kernel_1t1r<<<numBlocks,threadsPerBlock>>>(A_d, B_d, C_d, N);
    cudaEventRecord(stop, 0);
 
    cudaEventSynchronize(stop);
@@ -125,11 +103,13 @@ int main(void) {
    cudaEventDestroy(start);
    cudaEventDestroy(stop);
    printf("GPU Run TIme threadsrow %.2f ms \n", elapsed);
+*/
 ////////////////////////////////////Thread Column
+/*
    cudaEventCreate(&start);
    cudaEventCreate(&stop);
    cudaEventRecord(start, 0);
-   kernel_1t1c<<<numBlocks,threadsPerBlock>>>(A_d, B_d, C_d, RNC);
+   kernel_1t1c<<<numBlocks,threadsPerBlock>>>(A_d, B_d, C_d, N);
    cudaEventRecord(stop, 0);
 
    cudaEventSynchronize(stop);
@@ -137,19 +117,20 @@ int main(void) {
    cudaEventDestroy(start);
    cudaEventDestroy(stop);
    printf("GPU Run TIme threadscol %.2f ms \n", elapsed);
+*/
 //////////////////////////////////////Mem Copy
-   cudaMemcpy(A, A_d, (RNC*RNC)*sizeof(int), cudaMemcpyDeviceToHost);
+   cudaMemcpy(A, A_d, (N*N)*sizeof(float), cudaMemcpyDeviceToHost);
 
 /////////////////////////////////////Print matrix A
-/*
-   for (i = 0; i < RNC; i++){
-      for (j = 0; j < RNC; j++) {
-         printf("%d  ", A[i][j]);
+
+   for (i = 0; i < N; i++){
+      for (j = 0; j < N; j++) {
+         printf("%f  ", A[i][j]);
       }
       printf("\n");
    }
    printf("\n");
-*/
+	
 /////////////////////////////////////Free up memory
    cudaFree(A_d); 
    cudaFree(B_d); 
